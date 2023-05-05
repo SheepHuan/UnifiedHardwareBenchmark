6f33f366f3f0ea8d3a72bbdaf54088d7299d6476
SheepHuan <sheephuan@outlook.com>
2023-05-05 15:05:44 +0800

    TODO: 等待实现

---
+++ python/README.md
@@ -0,0 +1,5 @@
+# ONNX Node Generator
+
+# Doc
+1. [ONNX 1.15.0](https://onnx.ai/onnx/api/index.html)
+2. ONNXRUNTIME 
---
+++ python/adb_backend/connect2backend.py
@@ -0,0 +1,10 @@
+"""
+1. 先暴力生成onnx模型到/mnt/scrard
+2. 基于benchmark获得数据
+3. 获取到数据后集中分析
+"""
+from ppadb.client import Client as AdbClient
+# Default is "127.0.0.1" and 5037
+client = AdbClient(host="127.0.0.1", port=5037)
+device = client.device("3a9c4f5")
+device.shell("echo hello world !")
